3a16e2af3ede5856383d4f601010b59bc296ab00
Adharsh Jolly <116419763+AJ-CodesOfficial@users.noreply.github.com>
2022-10-27 21:43:10 +0530

    Project setup complete

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon clone</title>
+      </Head>
+
+      <h1>Ready To Code</h1>
+    </div>
+  );
+}
