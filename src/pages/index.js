9a69d887f09c18e1e1cc07e14925583f34f98601
Adharsh Jolly <116419763+AJ-CodesOfficial@users.noreply.github.com>
2022-10-28 20:25:01 +0530

    Header Component Completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <title>Amazon clone</title>
       </Head>
 
-      <h1>Ready To Code</h1>
+      <Header />
     </div>
   );
 }
